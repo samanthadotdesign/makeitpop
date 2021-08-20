a59482b0ab155d4c1ba7e08a8b9cffa4578ced20
samanthadotdesign <samleeml@gmail.com>
2021-08-20 15:23:37 +0800

    added text input functionality

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,26 +1,29 @@
 import React, { useContext } from 'react';
-import P5Wrapper from 'react-p5-wrapper';
+import Sketch from 'react-p5';
 import { DjContext } from '../../store';
 
 export default function GraphicControl() {
   const { textDispatch, textStoreState } = useContext(DjContext);
   const { input } = textStoreState;
+  let canvas;
+
+  // Rendering the setup canvas once
+  const setup = (p5) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+  };
 
   // Rerendering the sketch
-  const sketch = (p5, textInput) => {
-    p5.setup = () => {
-      p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    };
-
-    p5.draw = () => {
-      p5.clear();
-      p5.text(textInput, 100, p5.windowHeight / 2);
-    };
+  const draw = (p5) => {
+    p5.clear();
+    p5.text(input, 100, p5.windowHeight / 2);
   };
 
   return (
     <>
-      <P5Wrapper sketch={(p5) => { sketch(p5, input); }} />
+      <Sketch
+        setup={setup}
+        draw={draw}
+      />
     </>
   );
 }
