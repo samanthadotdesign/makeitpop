7e89b96ac2951ded86a1952c1c604d553693b7e9
samanthadotdesign <samleeml@gmail.com>
2021-08-27 16:16:27 +0800

    added grifinito type styles

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,32 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle, css } from 'styled-components';
+import GrifinitoRegular from './fonts/Grifinito-Regular.woff2';
+import GrifinitoMedium from './fonts/Grifinito-Medium.woff2';
 
 export const Main = styled.div`
+`;
+
+const fonts = css`
+  @font-face {
+    font-family: 'Grifinito Regular';
+    src: url(${GrifinitoRegular}) format('woff2');
+    font-weight: 400;
+  } 
+
+  @font-face {
+    font-family: 'Grifinito Medium';
+    src: url(${GrifinitoMedium}) format('woff2');
+    font-weight: 600;
+  } 
+
+  html {
+    font-family: 'Grifinito Regular';
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+  ${fonts}
 `;
